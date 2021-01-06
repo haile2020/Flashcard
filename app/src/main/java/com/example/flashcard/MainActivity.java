f4c9050b02270c09bbb8f2b6469da05d988bd860
haile <aryamhaile2019@gmail.com>
2021-01-06 10:31:07 -0800

    toggle the flashcard between the question side and the answer side Style the question and answer side of the card to better distinguish between the two sides Add selectable multiple choice answers beneath the card
        Change the background color of the multiple choice answers when clicked to indicate whether the question was answered correctly
    
    Further customize and style the card

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -4,28 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+    View view;
+    boolean isShowingAnswer = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view = this.getWindow().getDecorView();
+        view.setBackgroundResource(R.color.purple_700);
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
-                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
-
             }
         });
+
+
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setBackgroundResource(R.color.green_rest);
+                 }
+        });
+        findViewById(R.id.flashcard_answer1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer1).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer1).setBackgroundResource(R.color.amber_dark);
+               }
+        });
 
+        findViewById(R.id.flashcard_answer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer2).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer2).setBackgroundResource(R.color.amber_dark);
             }
         });
+
+        final ImageView button_eye = (ImageView) findViewById(R.id.eye_off);
+        button_eye.setOnClickListener(new View.OnClickListener() {
+            boolean isShowingAnswer = true;
+            public void onClick(View v) {
+                if (isShowingAnswer == true) {
+                    findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answer1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answer2).setVisibility(View.INVISIBLE);
+                    button_eye.setImageResource(R.drawable.ic_iconmonstr_eye_thin);
+                    isShowingAnswer = false;
+                } else if (isShowingAnswer == false) {
+                    findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer2).setVisibility(View.VISIBLE);
+                    button_eye.setImageResource(R.drawable.ic_iconmonstr_eye_off_thin);
+                    isShowingAnswer = true;
+                }
+            }
+        });
+
     }
 }
